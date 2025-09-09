1448b1d651b6bdd4f285f939a8b1f324f72a5c39
caio medeiros <caiokakalima10@gmail.com>
2025-09-09 17:55:32 -0300

    backcolor to green

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,10 @@
     import React from 'react'
     import Logo from '../../../src/assets/images/dossel-logo.png'
-
+    import '../header/Header.css'
     function Header() {
       return (
-        <div>
+        <div className='headercontainer'>
           <img src={Logo} alt="logo dossel" className='logo'/>
-
         </div>
       )
     }
--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -3,7 +3,7 @@ import Video from '../../../src/assets/images/video.mp4'
 
 function App() {
   return (
-    <video src={Video} autoPlay loop muted width="300"></video>
+    <video src={Video} autoPlay loop muted width="300" className='video'></video>
   );
 }
 
