6d1ac3d6cb7227537209d8baa837dd6a0977392d
caio medeiros <caiokakalima10@gmail.com>
2025-09-22 16:57:42 -0300

    fixing header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,22 @@
-    import React from 'react'
-    import Logo from '../../../src/assets/images/dossel-logo.png'
-    import '../header/Header.css'
-    function Header() {
-      return (
-        <div className='headercontainer'>
-        </div>
-      )
-    }
+import React from 'react'
+import Logo from '../../../src/assets/images/dossel-logo.png'
+import '../header/Header.css'
+import tucano from '../../assets/images/selective-focus-shot-toucan-standing-tree-branch.jpg'
 
-    export default Header
+function Header() {
+  return (
+  <div className='header'>
+      <img src={Logo} alt='dossel' className='logo' />
+      <nav className='nav'>
+        <a href='#inicio'>início</a>
+        <a href='#sobre'>sobre</a>
+        <a href='#setores'>setores</a>
+        <a href='#servicos'>serviços</a>
+        <a href='#impacto'>impacto</a>
+        <a href='#contato' className='cta'>contato</a>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
